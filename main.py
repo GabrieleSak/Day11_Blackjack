61ee61caeb72c83d388891e65600531aade27da8
GabrieleSak <gabriele.sakalauskiene@yahoo.com>
2022-09-19 13:06:51 +0300

    Detect users's or computer's blackjack, add ace calculation (value of 11 but if total score > 21 then is 1)

--- main.py
+++ main.py
@@ -15,6 +15,12 @@ import random
 from art import logo
 
 
+def score_calculation(cards):
+    if 11 in cards and sum(cards) > 21:
+        score = sum(cards) - cards.count(11) * 10
+    else:
+        score = sum(cards)
+    return score
 
 
 def blackjack():
@@ -25,18 +31,22 @@ def blackjack():
     players_cards = []
     computers_cards = []
     computer_score = 0
+    another_card = True
 
-    while computer_score < 17:
-        computers_cards.append(random.choice(cards))
-        computer_score = sum(computers_cards)
+    computers_cards.append(random.choice(cards))
+    computers_cards.append(random.choice(cards))
+    computer_score = score_calculation(computers_cards)
+
+
+
+    if (10 in computers_cards) and (11 in computers_cards) and computer_score == 21:
+        another_card = False
 
     players_cards.append(random.choice(cards))
 
-    another_card = True
-
     while another_card:
         players_cards.append(random.choice(cards))
-        player_score = sum(players_cards)
+        player_score = score_calculation(players_cards)
         print(f"Your cards: {players_cards}, current score: {player_score}")
         print(f"Computer's first card: {computers_cards[0]}")
         if player_score < 21:
@@ -48,13 +58,21 @@ def blackjack():
         else:
             another_card = False
 
+        while computer_score < 17:
+            computers_cards.append(random.choice(cards))
+            computer_score = score_calculation(computers_cards)
+
     print(f"Your final hand: {players_cards}, final score: {player_score}")
     print(f"Computer's final hand: {computers_cards}, final score: {computer_score}")
 
-    if player_score > 21 or (player_score < computer_score <= 21):
+    if (10 in computers_cards) and (11 in computers_cards) and computer_score == 21:
+        print("Blackjack! Computer wins 😤")
+    elif player_score > 21 or (player_score < computer_score <= 21) or (player_score == computer_score == 21):
         print("You lose 😤")
     elif player_score == computer_score:
         print("Draw")
+    elif (10 in players_cards) and (11 in players_cards) and player_score == 21:
+        print("Blackjack! You win 😃")
     else:
         print("You win 😃")
 
