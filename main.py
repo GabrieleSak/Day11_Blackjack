3e29c24f8c737035b6296916a1d122d34918461e
GabrieleSak <gabriele.sakalauskiene@yahoo.com>
2022-09-19 11:56:13 +0300

    Add recursive function so player can choose to play again

--- main.py
+++ main.py
@@ -14,29 +14,55 @@ import random
 
 from art import logo
 
-print(logo)
 
-cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
-players_cards = []
-computers_cards = []
-computer_score = 0
 
-while computer_score < 17:
-    computers_cards.append(random.choice(cards))
-    computer_score = sum(computers_cards)
+def blackjack():
+    print(logo)
 
-players_cards.append(random.choice(cards))
+    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
-another_card = True
+    players_cards = []
+    computers_cards = []
+    computer_score = 0
 
-while another_card:
+    while computer_score < 17:
+        computers_cards.append(random.choice(cards))
+        computer_score = sum(computers_cards)
+
     players_cards.append(random.choice(cards))
-    player_score = sum(players_cards)
-    print(f"Your cards: {players_cards}, current score: {player_score}")
-    print(f"Computer's first card: {computers_cards[0]}")
-    move = input("Type 'y' to get another card, type 'n' to pass: ")
-    if move == 'y':
-        another_card = True
+
+    another_card = True
+
+    while another_card:
+        players_cards.append(random.choice(cards))
+        player_score = sum(players_cards)
+        print(f"Your cards: {players_cards}, current score: {player_score}")
+        print(f"Computer's first card: {computers_cards[0]}")
+        if player_score < 21:
+            move = input("Type 'y' to get another card, type 'n' to pass: ")
+            if move == 'y':
+                another_card = True
+            else:
+                another_card = False
+        else:
+            another_card = False
+
+    print(f"Your final hand: {players_cards}, final score: {player_score}")
+    print(f"Computer's final hand: {computers_cards}, final score: {computer_score}")
+
+    if player_score > 21 or (player_score < computer_score <= 21):
+        print("You lose 😤")
+    elif player_score == computer_score:
+        print("Draw")
     else:
-        another_card = False
+        print("You win 😃")
+
+    new_game = input("Do you want to play a game of Blackjack? Type 'y' or 'n':")
+    if new_game == 'y':
+        blackjack()
+
+
+new_game = input("Do you want to play a game of Blackjack? Type 'y' or 'n':")
+if new_game == 'y':
+    blackjack()
